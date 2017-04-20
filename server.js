a9cb5b37b7496c7c16a995598bf8214eeb52cbd0
yujuan <15809216911@163.com>
2017-04-20 22:01:50 +0800

    Modify the files

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+import path from 'path';
 
-app.use(express.static('./'));
+app.use(express.static(__dirname + '/public'));
+
+app.use('*', function (req, res) {
+  res.sendFile(path.resolve('./public/index.html'));
+});
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    entry: "./entry.js",
+    entry: __dirname + "/public/src/main.js",
     output: {
-        path: __dirname,
+        path: __dirname+'/public',
         filename: "bundle.js"
     },
     module: {
