304aa9b45ad6e0892607d46b0bed3d294271ab82
yujuan <15809216911@163.com>
2017-04-20 22:01:25 +0800

    Create the reducer to change state

--- public/src/reducers/handle-counter.js
+++ public/src/reducers/handle-counter.js
@@ -1,3 +1,14 @@
 /**
  * Created by yujuan on 17-4-20.
  */
+const handleCounter = (state = {count:0}, action)=> {
+  switch(action.type){
+    case 'COUNTER_INCRE':
+      return Object.assign({}, state, { count: state.count + 1});
+    case 'COUNTER_DECRE':
+      return Object.assign({}, state, { count: state.count -1});
+  }
+  return state;
+};
+
+export default handleCounter;
