365b0547fc36c4ef98c9769d5e1d2e54b3f45fbd
yujuan <15809216911@163.com>
2017-04-25 14:49:06 +0800

    Add combineReducers

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -1,3 +1,20 @@
 /**
  * Created by yujuan on 17-4-24.
  */
+import handleCounter from './handle-counter';
+
+function combineReducers(reducers) {
+  return (state = {}, action)=> {
+    return Object.keys(reducers).reduce((nextState, key)=> {
+      nextState = reducers[key](state[key],action);
+      console.log(nextState);
+      return nextState;
+    }, {})
+  }
+}
+
+const  reducers = combineReducers({
+  handleCounter
+});
+
+export default reducers;
